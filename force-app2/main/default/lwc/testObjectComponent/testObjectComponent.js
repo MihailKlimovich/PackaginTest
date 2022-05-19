d0b3ecaabbdae55b4156c51e0e40b002eb9c6501
MihailKlimovich <mishaklimovich87@gmail.com>
2022-05-19 15:29:55 +0300

    Task2 Commit4

--- force-app2/main/default/lwc/testObjectComponent/testObjectComponent.js
+++ force-app2/main/default/lwc/testObjectComponent/testObjectComponent.js
@@ -1,3 +1,22 @@
 import { LightningElement } from 'lwc';
+import createTestCustomObject from '@salesforce/apex/TestCustomObjectController.createTestCustomObject';
 
-export default class TestObjectComponent extends LightningElement {}
+export default class TestObjectComponent extends LightningElement {
+    nameObject;
+    resultInsert;
+
+    handleChange(event) {
+        this.nameObject = event.target.value;
+    }
+    handleClick() {
+        createTestCustomObject({
+            nameObject : this.nameObject
+        }).then((result) => {
+            this.resultInsert = result;
+            console.log(this.resultInsert);
+        }).catch((error) => {
+            this.resultInsert = error;
+            console.log(this.resultInsert);
+        });
+    }
+}
