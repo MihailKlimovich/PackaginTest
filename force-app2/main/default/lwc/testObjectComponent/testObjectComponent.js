82b417faca5b44b1c06f5b91dad442e7769730e5
MihailKlimovich <mishaklimovich87@gmail.com>
2022-05-20 13:54:59 +0300

    Task2 Commit6

--- force-app2/main/default/lwc/testObjectComponent/testObjectComponent.js
+++ force-app2/main/default/lwc/testObjectComponent/testObjectComponent.js
@@ -1,5 +1,6 @@
 import { LightningElement } from 'lwc';
 import createTestCustomObject from '@salesforce/apex/TestCustomObjectController.createTestCustomObject';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class TestObjectComponent extends LightningElement {
     nameObject;
@@ -13,10 +14,19 @@ export default class TestObjectComponent extends LightningElement {
             nameObject : this.nameObject
         }).then((result) => {
             this.resultInsert = result;
-            console.log(this.resultInsert);
+            this.showToast();
         }).catch((error) => {
             this.resultInsert = error;
             console.log(this.resultInsert);
         });
+    }
+    showToast() {
+        const event = new ShowToastEvent({
+            title: 'Success result',
+            message: 'You add new TestCustomObject record',
+            variant: 'success',
+            mode: 'dismissable'
+        });
+        this.dispatchEvent(event);
     }
 }
